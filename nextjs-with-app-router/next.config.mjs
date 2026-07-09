90b38ea5addeea6e2efbb134371a6593a6c602c2
techking11 <nma1132000@gmail.com>
2026-07-09 14:37:02 +0400

    add: login page

--- nextjs-with-app-router/next.config.mjs
+++ nextjs-with-app-router/next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'fakestoreapi.com',
         pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        pathname: '**'
       }
     ]
   }
--- nextjs-with-app-router/src/app/dashboard/(auth)/login/page.jsx
+++ nextjs-with-app-router/src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Button from '@/components/button/Button'
+import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Login() {
   return (
-    <div>
+    <div className={styles.login}>
       <h1>Login</h1>
+      <form className={styles.form}>
+        <input type="text" placeholder="eg: John Doe" />
+        <input type="password" placeholder="eg: 123456" />
+        <Button type="submit">Login</Button>
+      </form>
+      <button className={styles.googleLogin}>
+        <Image src="https://img.icons8.com/color/1200/google-logo.jpg" alt="Google" width={20} height={20} />
+      </button>
+
+      <p className={styles.register}>Don't have an account? <Link href="/dashboard/register">Register</Link></p>
     </div>
   )
 }
