15472c158c48fcb427629d55cef51031bfdf63f2
techking11 <nma1132000@gmail.com>
2026-07-14 13:00:36 +0400

    add: register page

--- nextjs-with-app-router/src/app/dashboard/(auth)/register/page.jsx
+++ nextjs-with-app-router/src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Button from '@/components/button/Button'
+import styles from './page.module.css'
+import Link from 'next/link'
 
 function Register() {
   return (
-    <div>
+    <div className={styles.register}>
       <h1>Register</h1>
+      <form className={styles.form}>
+        <input type="text" placeholder="eg: John Doe" />
+        <input type="email" placeholder="eg: john.doe@example.com" />
+        <input type="password" placeholder="eg: 123456" />
+        <Button type="submit">Register</Button>
+      </form>
+      <p className={styles.loginLink}>Already have an account? <Link href="/dashboard/login">Login</Link></p>
     </div>
   )
 }
